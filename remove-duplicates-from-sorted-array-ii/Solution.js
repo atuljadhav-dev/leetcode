a6466a71a2271bacb15b05e651a26ff9c6d611b3
Atul Mohan Jadhav <atulj9537@gmail.com>
2025-11-10 20:22:53 +0530

    Update solution for Remove Duplicates from Sorted Array II

--- remove-duplicates-from-sorted-array-ii/Solution.js
+++ remove-duplicates-from-sorted-array-ii/Solution.js
@@ -3,11 +3,11 @@
  * @return {number}
  */
 var removeDuplicates = function (nums) {
-    const len=nums.length
-    if (len <= 2) return len;
+    const l=nums.length;
+    if (l <= 2) return l;
 
     let j = 2;
-    for (let i = 2; i < len; i++) {
+    for (let i = 2; i < l; i++) {
 
         if (nums[i] !== nums[j - 2]) {
             nums[j++] = nums[i];
