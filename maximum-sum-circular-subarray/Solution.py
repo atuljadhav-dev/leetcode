3f1915805e23e32a038608e3122500290880d37f
Atul Mohan Jadhav <atulj9537@gmail.com>
2026-06-30 16:57:43 +0530

    Add solution for Maximum Sum Circular Subarray

---
+++ maximum-sum-circular-subarray/Solution.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def maxSubarraySumCircular(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        max_ending = nums[0]
+        max_res = nums[0]
+        for i in range(1, len(nums)):
+            v1 = nums[i]
+            v2 = max_ending + nums[i]
+            max_ending = max(v1, v2)
+            max_res = max(max_res, max_ending)
+        min_ending = nums[0]
+        min_res = nums[0]
+        for i in range(1, len(nums)):
+            v1 = nums[i]
+            v2 = min_ending + nums[i]
+            min_ending = min(v1, v2)
+            min_res = min(min_res, min_ending)
+        if max_res<0:
+            return max_res 
+        total = sum(nums)
+        return max(max_res,total-min_res)
