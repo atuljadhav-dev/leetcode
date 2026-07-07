8c2ab4c10bac1c49d8b2ec5edb20dd49567dc9d8
Atul Mohan Jadhav <atulj9537@gmail.com>
2026-07-07 17:54:24 +0530

    Update solution for Contains Duplicate II

--- contains-duplicate-ii/Solution.py
+++ contains-duplicate-ii/Solution.py
@@ -2,7 +2,7 @@ class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
         idx={}
         for i in range(0, len(nums)):
-            if abs(idx.get(nums[i],float('inf'))-i)<=k:
+            if nums[i] in idx and i-idx[nums[i]]<=k:
                 return True
             idx[nums[i]]=i
         return False
