3403fd28ee950028e62e3715634b96392567d17b
Atul Mohan Jadhav <atulj9537@gmail.com>
2026-07-07 16:54:05 +0530

    Update solution for Majority Element

--- majority-element/Solution.py
+++ majority-element/Solution.py
@@ -1,13 +1,11 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
-        ans=nums[0]       
-        count=0
-        for i in range(1,len(nums)):
-            if nums[i]==ans:
-                count+=1
-            else:
-                count-=1
-            if count<0:
-                ans=nums[i]
+        count={}
+        ans=nums[0]
+        max=0
+        for i in nums:
+            count[i]=count.get(i, 0)+1
+            if count[i]>max:
+                ans=i
+                max=count[i]
         return ans
