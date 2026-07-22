f6c21bdbdbc6f70780978289c38afda696c7f6ee
Atul Mohan Jadhav <atulj9537@gmail.com>
2026-07-22 21:36:20 +0530

    Add solution for Plus One

---
+++ plus-one/Solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        ans = []
+        carry = 1
+
+        for i in range(len(digits) - 1, -1, -1):
+            total = digits[i] + carry
+            carry = total // 10
+            ans.append(total % 10)
+
+        if carry:
+            ans.append(carry)
+
+        ans.reverse()
+        return ans
