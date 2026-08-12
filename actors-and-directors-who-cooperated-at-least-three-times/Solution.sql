67e4db545654c865efa5f360a7470390fdf3d6a7
Atul Mohan Jadhav <atulj9537@gmail.com>
2026-08-12 15:41:16 +0530

    Add solution for Actors and Directors Who Cooperated At Least Three Times

---
+++ actors-and-directors-who-cooperated-at-least-three-times/Solution.sql
@@ -0,0 +1,7 @@
+/* Write your PL/SQL query statement below */
+-- select actor_id,director_id from ActorDirector
+-- group by actor_id,director_id
+-- having count(*)>2;
+select actor_id,director_id from ActorDirector
+group by actor_id,director_id
+having count(*)>2;
