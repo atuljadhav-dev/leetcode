66a9c36aee53ebb1a8a2316e3eb709414721c36e
Atul Mohan Jadhav <atulj9537@gmail.com>
2026-09-03 14:54:36 +0530

    Add solution for Average Selling Price

---
+++ average-selling-price/Solution.sql
@@ -0,0 +1,7 @@
+/* Write your PL/SQL query statement below */
+select p.product_id,nvl(round(sum(p.price*u.units)/sum(u.units),2),0) average_price
+from prices p 
+left join unitssold u
+on p.product_id=u.product_id 
+and u.purchase_date between p.start_date and p.end_date
+group by p.product_id
