578ef8a7f9db6600794d60d40cb31aef08393322
Atul Mohan Jadhav <atulj9537@gmail.com>
2026-09-05 11:49:00 +0530

    Add solution for Tree Node

---
+++ tree-node/Solution.sql
@@ -0,0 +1,8 @@
+/* Write your PL/SQL query statement below */
+select id,
+case
+when p_id is null then 'Root'
+when id in (select distinct p_id from Tree) then 'Inner'
+else 'Leaf'
+end as "type"
+from Tree
