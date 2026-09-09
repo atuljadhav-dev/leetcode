46807d6668adc4b2c78c6c30dd4e92c4f4ffe7ce
Atul Mohan Jadhav <atulj9537@gmail.com>
2026-09-09 19:03:50 +0530

    Add solution for Calculate Special Bonus

---
+++ calculate-special-bonus/Solution.sql
@@ -0,0 +1,8 @@
+/* Write your PL/SQL query statement below */
+select 
+    employee_id,
+    case when mod(employee_id,2)!=0 
+    and name not like 'M%' 
+    then salary else 0 end bonus
+from employees 
+order by employee_id
